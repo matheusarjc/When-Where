1298c6f8bb7774f316c1d7e378eca8d1a559df2d
Matheus Araujo <carvalhom630@gmail.com>
2025-01-25 19:31:25 -0300

    chore: first config

---
+++ src/styles/theme/theme.ts
@@ -0,0 +1,13 @@
+export const theme = {
+  colors: {
+    primary: "#0070f3",
+    secondary: "#005bb5",
+    background: "#f0f0f0",
+    text: "#333",
+  },
+  fontSizes: {
+    small: "12px",
+    medium: "16px",
+    large: "20px",
+  },
+};
